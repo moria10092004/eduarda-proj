57e8ee280128fc4ac39cb7ef22903481252adcc5
moria10092004 <102737558+moria10092004@users.noreply.github.com>
2022-03-31 09:06:19 -0400

    Add files via upload

---
+++ Projeto1/Calculadora.xaml.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projeto1
+{
+    /// <summary>
+    /// Lógica interna para Calculadora.xaml
+    /// </summary>
+    public partial class Calculadora : Window
+    {
+        decimal calculo;
+        bool adicao = false;
+        bool subtracao = false;
+        bool multiplicacao = false;
+        bool divisao = false;
+        bool resultado = false;
+        bool porcentagem = false;
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_Um_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resposta.Text += "1";
+            txt_Operação.Text += "1";
+
+        }
+
+        private void bt_Dois_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resposta.Text += "2";
+            txt_Operação.Text += "2";
+        }
+
+        private void bt_Tres_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resposta.Text += "3";
+            txt_Operação.Text += "3";
+        }
+
+        private void bt_Quatro_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resposta.Text += "4";
+            txt_Operação.Text += "4";
+        }
+
+        private void bt_Cinco_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resposta.Text += "5";
+            txt_Operação.Text += "5";
+        }
+
+        private void bt_Seis_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resposta.Text += "6";
+            txt_Operação.Text += "6";
+        }
+
+        private void bt_Sete_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resposta.Text += "7";
+            txt_Operação.Text += "7";
+        }
+
+        private void bt_Oito_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resposta.Text += "8";
+            txt_Operação.Text += "8";
+        }
+
+        private void bt_Nove_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resposta.Text += "9";
+            txt_Operação.Text += "9";
+        }
+
+        private void bt_Mais_Click(object sender, RoutedEventArgs e)
+        {
+            calculo = Convert.ToDecimal(txt_Resposta.Text);
+
+            adicao = true;
+            subtracao = false;
+            multiplicacao = false;
+            divisao = false;
+
+            txt_Operação.Text += "+";
+            txt_Resposta.Text = "";
+        }
+
+        private void bt_Igualdade_Click(object sender, RoutedEventArgs e)
+        {
+            resultado = true;
+            txt_Operação.Text += "=";
+
+            if(adicao == true)
+            {
+                txt_Resposta.Text = Convert.ToString(Convert.ToDecimal(txt_Resposta.Text) + calculo);
+                txt_Operação.Text += txt_Resposta.Text;
+            }
+            if (subtracao == true)
+            {
+                txt_Resposta.Text = Convert.ToString(calculo - Convert.ToDecimal(txt_Resposta.Text));
+                txt_Operação.Text += txt_Resposta.Text;
+            }
+            if (multiplicacao == true)
+            {
+                txt_Resposta.Text = Convert.ToString(Convert.ToDecimal(txt_Resposta.Text) * calculo);
+                txt_Operação.Text += txt_Resposta.Text;
+            }
+            if (divisao == true)
+            {
+                txt_Resposta.Text = Convert.ToString(calculo / Convert.ToDecimal(txt_Resposta.Text));
+                txt_Operação.Text += txt_Resposta.Text;
+            }
+           
+        }
+
+        private void bt_Limpar_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resposta.Text = "";
+            txt_Operação.Text = "";
+        }
+
+        private void bt_Subtracao_Click(object sender, RoutedEventArgs e)
+        {
+            calculo = Convert.ToDecimal(txt_Resposta.Text);
+
+            subtracao = true;
+            adicao = false;
+            multiplicacao = false;
+            divisao = false;
+            porcentagem = false;
+
+            txt_Operação.Text += "-";
+            txt_Resposta.Text = "";
+
+        }
+
+        private void bt_Multiplicacao_Click(object sender, RoutedEventArgs e)
+        {
+            calculo = Convert.ToDecimal(txt_Resposta.Text);
+
+            multiplicacao = true;
+            subtracao = false;
+            adicao = false;
+            divisao = false;
+            porcentagem = false;
+
+            txt_Operação.Text += "*";
+            txt_Resposta.Text = "";
+
+        }
+
+        private void bt_Divisao_Click(object sender, RoutedEventArgs e)
+        {
+            calculo = Convert.ToDecimal(txt_Resposta.Text);
+
+            divisao = true;
+            multiplicacao = false;
+            subtracao = false;
+            adicao = false;
+            porcentagem = false;
+
+            txt_Operação.Text += "/";
+            txt_Resposta.Text = "";
+
+        }
+
+        private void bt_Zero_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resposta.Text += "0";
+            txt_Operação.Text += "0";
+        }
+
+        private void bt_Ponto_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resposta.Text += ".";
+            txt_Operação.Text += ".";
+        }
+
+        private void bt_Virgula_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resposta.Text += ";";
+            txt_Operação.Text += ";";
+        }
+
+        private void bt_ApagUltimo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string apagar = txt_Resposta.Text;
+                apagar = apagar.Remove(apagar.Length - 1);
+                txt_Resposta.Text = apagar;
+                txt_Operação.Text = apagar;
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+
+        private void bt_Porcentagem_Click(object sender, RoutedEventArgs e)
+        {
+            //calculo = Convert.ToDecimal(txt_Resposta.Text);
+
+           // porcentagem = true;
+          //  divisao = false;
+           // multiplicacao = false;
+            //subtracao = false;
+           // adicao = false;
+
+           // txt_Operação.Text += "%";
+            //txt_Resposta.Text = "";
+        }
+    }
+}
---
+++ Projeto1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projeto1
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string login;
+        string senha;
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            login = Properties.Settings.Default.Login;
+            senha = Properties.Settings.Default.Senha;
+
+            if (txt_login.Text == login && pwSenha.Password == senha)
+            {
+                MessageBox.Show("LONGIN CONFIRMADO COM SUCESSO!");
+                Calculadora calcu = new Calculadora();
+                calcu.ShowDialog();
+            }
+            else
+            {
+               MessageBox.Show("LONGIN NEGADO!");
+               txt_login.Clear();
+               pwSenha.Clear();
+            }
+           
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Redefinir alterar = new Redefinir();
+            alterar.Show();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+           
+        }
+
+        private void bt_Fechar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
